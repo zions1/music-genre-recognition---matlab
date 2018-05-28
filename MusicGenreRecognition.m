63d2f182ac44b13b9f244e3920265bab042590dc
Wiktor Florencki <wiktor.florencki@gmail.com>
2018-05-28 11:04:04 +0100

    Fixed Music Genre Recognition main class. Removed data sets. Signed-off-by: Wiktor Florencki <wiktor.florencki@gmail.com>

--- MusicGenreRecognition.m
+++ MusicGenreRecognition.m
@@ -1,19 +1,20 @@
 addpath('C:\Program Files\MATLAB\R2017a\toolbox\utility')
 addpath('C:\Program Files\MATLAB\R2017a\toolbox\sap')
 addpath('C:\Program Files\MATLAB\R2017a\toolbox\machineLearning')
-addpath('C:\Program Files\MATLAB\R2017a\toolbox\machineLearning/externalTool/libsvm-3.21/matlab')	% For using SVM classifier
-
-auDir='C:/Users/Donski/Desktop/thesis/genres/genres_au';
+%addpath('C:\Program Files\MATLAB\R2017a\toolbox\machineLearning/externalTool/libsvm-3.21/matlab')	% For using SVM classifier
+extension = 'flac';
+auDir=['C:/Users/Donski/Desktop/thesis/genres/genres_', extension];
 opt=mmDataCollect('defaultOpt');
-opt.extName='au';
-auSet=mmDataCollect(auDir, opt, 1); 
+opt.extName=extension;
+auSet=mmDataCollect(auDir, opt, 1);
 
 fprintf('Platform: %s\n', computer);
 fprintf('MATLAB version: %s\n', version);
 fprintf('Script starts at %s\n', char(datetime));
 scriptStartTime=tic;	% Timing for the whole script
+dataSetFileName = ['ds_',extension,'.mat'];
 
-if ~exist('ds.mat', 'file')
+if ~exist(dataSetFileName, 'file')
 	myTic=tic;
 	opt=dsCreateFromMm('defaultOpt');
 	opt.auFeaFcn=@mgcFeaExtract;	% Function for feature extraction
@@ -21,17 +22,31 @@ if ~exist('ds.mat', 'file')
 	ds=dsCreateFromMm(auSet, opt, 1);
 	fprintf('Time for feature extraction over %d files = %g sec\n', length(auSet), toc(myTic));
 	fprintf('Saving ds.mat...\n');
-	save ds ds
+	save(dataSetFileName, ds)
 else
 	fprintf('Loading ds.mat...\n');
-	load au_ds.mat
+	load(dataSetFileName)
 end
+
+input = ds.input';
+desired = ds.output';
+outputNames= ds.outputName;
+trainSize = 0.8;
+rng(1)      %For reproducibility
+
+%---------- KNN ---------
+fprintf('\nK-Nearest Neighbors algorithm starts at %s\n', char(datetime));
+[train, test, trainDesired, testDesired] = createTrainAndTestSets(input, desired, trainSize);
+knn(test, testDesired, outputNames, 0);
+fprintf('K-Nearest Neighbors algorithm finished at %s\n', char(datetime));
+%------------------------
 
 
 
 %Test MFCC
-auFile=[auDir, '/metal/metal.00002.au'];
+auFile=[auDir, '/metal/metal.00002.', extension];
 figure;
-mgcFeaExtract(auFile, [], 1);
+mgcFeaExtract(auFile, [], 0);
 
+%figure; dsFeaVecPlot(ds); figEnlarge;
 return
